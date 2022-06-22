6628dbe6691847d89234e54d0a5a6a8a5d04fb24
Abd Y Enshasi <Enshasi@gmail.com>
2022-06-22 17:45:33 +0300

    Upload Files

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let Toggle = document.querySelector(".header .container .toggle-menu");
+let navbar = document.querySelector(".header .container .navbar");
+
+Toggle.onclick = () => {
+  navbar.classList.toggle("active");
+    };
